1ba880ad29906f52c2176f4f46a6491209692b95
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-09-20 16:39:25 -0400

    request notification permissions

--- taskly/app/counter/index.tsx
+++ taskly/app/counter/index.tsx
@@ -1,8 +1,15 @@
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 
 export default function CounterScreen() {
   const router = useRouter();
+  const handleRequestPermission = async () => {
+    const result = await registerForPushNotificationsAsync();
+    console.log("result", result);
+  };
+
   return (
     <View style={styles.container}>
       {/* <TouchableOpacity onPress={() => router.navigate("/idea")}>
@@ -10,6 +17,13 @@ export default function CounterScreen() {
           Go to /idea
         </Text>
       </TouchableOpacity> */}
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={handleRequestPermission}
+      >
+        <Text style={styles.buttonText}>Request permission</Text>
+      </TouchableOpacity>
       <Text style={styles.text}>Counter</Text>
     </View>
   );
@@ -24,5 +38,16 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    letterSpacing: 1,
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
 });
