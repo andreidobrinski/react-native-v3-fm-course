9a12876e4fb5b6d9ec78ea6b36738bd06ff79703
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-09-20 09:36:01 -0400

    add feature to create new shopping list item

--- taskly/app/index.tsx
+++ taskly/app/index.tsx
@@ -1,18 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { Link } from "expo-router";
+import { useState } from "react";
+
+type ShoppingListItem = {
+  id: string;
+  name: string;
+};
+
+const initialList: Array<ShoppingListItem> = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Sugar" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<Array<ShoppingListItem>>(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" /> */}
       {/* <Link href="/counter">Counter</Link>
       <Link href="/idea">Idea</Link> */}
-      <ShoppingListItem name="Coffee" isCompleted />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" />
+      <TextInput
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+        placeholder="E.g. coffee"
+        style={styles.textInput}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
     </View>
   );
 }
@@ -21,6 +56,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
