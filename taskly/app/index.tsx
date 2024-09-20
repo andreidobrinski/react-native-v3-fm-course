294ec0e1881f29e1076b4bd6eaaa9696712382ae
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-09-19 22:24:05 -0400

    add routing for pages

--- taskly/app/index.tsx
+++ taskly/app/index.tsx
@@ -2,11 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <Link href="/counter">Counter</Link>
+      <Link href="/idea">Idea</Link>
       <ShoppingListItem name="Coffee" isCompleted />
       <ShoppingListItem name="Tea" />
       <ShoppingListItem name="Sugar" />
