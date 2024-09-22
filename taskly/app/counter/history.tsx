22994e1c519d90ea2730f78fc0bd61666e3d4a50
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-09-22 17:26:27 -0400

    add timestamp history

--- taskly/app/counter/history.tsx
+++ taskly/app/counter/history.tsx
@@ -1,10 +1,42 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { countdownStorageKey, PersistedCountedownState } from ".";
+import { getFromStorage } from "../../utils/storage";
+import { format } from "date-fns";
+import { theme } from "../../theme";
+
+const fullDateFormat = `LLL d yyyy, h:mm aaa`;
 
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountedownState>();
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+    init();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      data={countdownState?.completedAtTimestamps}
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.listItemText}>
+            {format(item, fullDateFormat)}
+          </Text>
+        </View>
+      )}
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>No history</Text>
+        </View>
+      }
+    />
   );
 }
 
@@ -16,5 +48,27 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+  contentContainer: {
+    marginTop: 8,
+  },
+  listItem: {
+    backgroundColor: theme.colorLightGrey,
+    marginHorizontal: 8,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 8,
+  },
+  listEmptyContainer: {
+    marginVertical: 18,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- taskly/app/counter/index.tsx
+++ taskly/app/counter/index.tsx
@@ -19,9 +19,9 @@ import { getFromStorage, saveToStorage } from "../../utils/storage";
 // 10 seconds
 const frequency = 10 * 1000;
 
-const countdownStorageKey = "taskly-countdown";
+export const countdownStorageKey = "taskly-countdown";
 
-type PersistedCountedownState = {
+export type PersistedCountedownState = {
   currentNotificationId: string | undefined;
   completedAtTimestamps: Array<number>;
 };
